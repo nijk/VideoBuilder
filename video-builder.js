451a2285b730e7de73c3a65d3e8279ede685cd7b
Nick Aspinall <nick@codenamenick.co.uk>
2017-04-20 12:12:06 +0100

    Fix base64 dependency

--- video-builder.js
+++ video-builder.js
@@ -10,7 +10,7 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['b'], factory);
+    define(['base64'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
